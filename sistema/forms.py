5bb6824cf06387792f9b8c61bfd15a7827a8a644
Fernando <gabrielfcassuncao@gmail.com>
2026-05-03 11:32:37 -0300

    formularios principais finalizados

--- sistema/forms.py
+++ sistema/forms.py
@@ -20,6 +20,62 @@ import os
 # segurança no bd
 from werkzeug.utils import secure_filename
 
+# formulario de cadastro
+class CadastroForm(FlaskForm):
+    nome = StringField('Nome', validators=[DataRequired()])
+    sobrenome = StringField('Nome', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    confirmacao_senha = PasswordField('Confirmar Senha', validators=[DataRequired(), EqualTo('senha')])
+    btnsubmit = SubmitField('Salvar')
+
+    # criando o validador
+    def validate_email(self, email):
+        if Usuario.query.filter_by(email=email.data).first(): # busca na tabela usuario, na coluna email, o email enviado
+            return ValidationError('Usuario já cadastrado com esse Email!!') # resposta do erro
+
+    # criando o save
+    def save(self):
+        # criptografando a senha
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+
+        # criando o usuario
+        usuario = Usuario(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+
+        # salvando
+        db.session.add(usuario)
+        db.session.commit()
+
+        # retorno o usuario para fazer login
+        return usuario
+    
+
+# formulario de login
+class LoginForm(FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    btnsubmit = SubmitField('Enviar')
+
+    # função de logar
+    def login(self):
+        # recupera o usuario
+        user = Usuario.query.filter_by(email=self.email.data).first()
+
+        # verificando a senha
+        if user:
+            if bcrypt.check_password_hash(user.senha, self.senha.data.encode('utf-8')):
+                # retorna o usuario
+                return user
+            else:
+                raise Exception("Senha incorreta!!")
+        else:
+            return None
+        
 # cadastrar atleta
 class CadastroAtletaForm(FlaskForm):
     nome = StringField('Nome', validators=[DataRequired()])
